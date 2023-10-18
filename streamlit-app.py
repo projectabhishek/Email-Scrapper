2c1bc0dc646e1033c854b9e659bf1ada261970a9
projectabhishek <148330024+projectabhishek@users.noreply.github.com>
2023-10-18 14:49:36 +0530

    Add files via upload

--- streamlit-app.py
+++ streamlit-app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 
-from selenium.webdriver.support.ui import WebDriverWait
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
